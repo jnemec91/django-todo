36f4cff12d8c9083110473ae471833f17db2a1cf
Jaroslav Nemec <j.nemecc9@gmail.com>
2023-08-24 17:07:21 +0200

    Implement django auth user model, login and logout views, create pages for showing stickers and style them.

--- todo_list/todo_list_app/urls.py
+++ todo_list/todo_list_app/urls.py
@@ -5,7 +5,7 @@ app_name = 'todo_list_app'
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('login/', views.login, name='login'),
+    path('login/', views.user_login, name='login'),
     path('signup/', views.signup, name='signup'),
-    path('logout/', views.logout, name='logout')
+    path('logout/', views.log_out, name='logout')
 ]
--- todo_list/todo_list_app/views.py
+++ todo_list/todo_list_app/views.py
@@ -1,16 +1,43 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+from .models import TodoList, TodoField, UserOptions
 
 # Create your views here.
 
 def index(request):
-    return render(request, 'todo_list/index.html')
+    if request.user.is_authenticated:
+        todo_lists = [{'name':i.name, 'owner':i.owner, 'fields':[a for a in i.fields.all()]} for i in TodoList.objects.filter(owner=request.user)]
+        
+        return render(request, 'todo_list/index.html', {'todo_lists':todo_lists})
+    
+    else:
+        return redirect('todo_list_app:login')
 
-def login(request):
+
+def user_login(request):
+    if request.method =='POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        print(username, password)
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('todo_list_app:index')
+        else:
+            messages.add_message(request, messages.ERROR, 'You`ve entered a wrong username or password.')
+            return redirect('todo_list_app:login')
+   
     return render(request, 'todo_list/login.html')
 
 def signup(request):
     return render(request, 'todo_list/signup.html')
 
-def logout(request):
-    return render(request, 'todo_list/logout.html')
+@login_required
+def log_out(request):
+    if request.user.is_authenticated:
+        logout(request)
+        return redirect('todo_list_app:login')
 
