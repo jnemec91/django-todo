a13a6b14125e716f3ae15432ce380766ba6e3f89
Jaroslav Nemec <j.nemecc9@gmail.com>
2023-09-14 12:23:16 +0200

    add urls for all views

--- todo_list/todo_list_app/urls.py
+++ todo_list/todo_list_app/urls.py
@@ -7,5 +7,12 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('login/', views.user_login, name='login'),
     path('signup/', views.signup, name='signup'),
-    path('logout/', views.log_out, name='logout')
+    path('logout/', views.log_out, name='logout'),\
+    path('create_todo_list/', views.create_todo_list, name='create_todo_list'),
+    path('edit_todo_list/<int:todo_list_id>/', views.edit_todo_list, name='edit_todo_list'),
+    path('delete_todo_list/<int:todo_list_id>/', views.delete_todo_list, name='delete_todo_list'),
+    path('list_of_todo_lists/', views.list_of_todo_lists, name='list_of_todo_lists'),
+    path('todo_list/<str:todo_list_hash>/', views.list_details, name='list_details'),
+    path('check/<int:todo_field_id>',views.check_task, name='check_task'),
+    path('settings/', views.settings, name='settings'),
 ]
