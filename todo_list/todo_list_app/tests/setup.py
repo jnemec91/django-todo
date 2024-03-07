dfe44bd2d77c89bd4a313f415bb5fb9c7126f474
Jaroslav Nemec <j.nemecc9@gmail.com>
2024-03-07 13:09:57 +0100

    add another user to setup file

--- todo_list/todo_list_app/tests/setup.py
+++ todo_list/todo_list_app/tests/setup.py
@@ -8,7 +8,7 @@ class TestSetup:
     """
     Class for setting up tests.
     """
-    
+
     def __init__(self) -> None:
         # create standart user
         self.user = User.objects.create_user(
@@ -24,6 +24,25 @@ class TestSetup:
             dark_mode=False,
             font_style='1',
             )
+        
+
+        # create another user
+        self.another_user = User.objects.create_user(
+            email='another@test.com',
+            username='anotheruser',
+            first_name='another',
+            last_name='another',
+            password='12345',
+            )
+        
+        self.user_options = UserOptions.objects.create(
+            user=self.user,
+            dark_mode=False,
+            font_style='1',
+            )
+
+        
+
         
         # create django test client
         self.client =  Client()
